89e570675f077df74db4b699ccb852d4012d48d3
Karol Musur <kamusur@gmail.com>
2026-07-12 21:43:46 +0200

    Bump version to v0.6.2

--- pro.go
+++ pro.go
@@ -28,7 +28,7 @@ func main() {
 	app := &cli.App{
 		Name:    "pro",
 		Usage:   "Pull Request Opener",
-		Version: "v0.6.1",
+		Version: "v0.6.2",
 		Flags:   openCommandFlags,
 		Commands: []*cli.Command{
 			{
